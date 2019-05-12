7414a1d64f7d68fba50cf8feb033019e846b13c9
Book <tekliebelay@gmail.com>
2019-05-12 21:28:48 +0200

    Count of needed resources added

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
         System.out.println("Starting to make a coffee");
@@ -7,6 +9,17 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready");
+        System.out.println("Write how many cups of coffee you will need:");
+        Scanner in = new Scanner(System.in);
+        int numOfCups = in.nextInt();
+       countNeededResources(numOfCups);
+    }
+    private static void countNeededResources(int numOfCups){
+        System.out.println("For"+ numOfCups + "cups of coffee you will need:");
+        System.out.println(numOfCups * 200 +" ml of water");
+        System.out.println(numOfCups * 50 +" ml of milk");
+        System.out.println(numOfCups * 15 +" ml of coffee beans");
+
     }
 }
 
